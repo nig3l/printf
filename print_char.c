af6fbb82c5cbebb353cfe4cc2f5a97c875f58c54
nigel <nigelchimwene@gmail.com>
2022-08-10 02:18:07 -0700

    task 1

---
+++ print_char.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_c - prints char
+ * @c: char to be printed
+ *
+ * Return: always 1
+ */
+
+int print_c(va_list c)
+{
+	char ch = (char)va_arg(c, int);
+
+	_putchar(ch);
+	return (1);
+}
+
+/**
+ * print_s - prints a string
+ * @s: string to be printed
+ *
+ * Return: number of chars printed
+ */
+
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
+}
